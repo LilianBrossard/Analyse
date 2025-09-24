f6a30b8339c62ed8f9ee60347ad38f9c51fbe272
AntoineToullec <148762390+AntoineToullec@users.noreply.github.com>
2025-09-24 10:16:06 +0200

    Implement PCA analysis and biplot visualization for scaled data

--- projet.py
+++ projet.py
@@ -6,3 +6,20 @@ from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 
 data = pd.read_csv('data.csv')
+
+temp = x.sub(x.mean())
+x_scaled = temp.div(x.std())
+
+pca = PCA(n_components=3)
+pca.fit(x_scaled)
+
+pca_res = pca.fit_transform(x_scaled)
+
+
+//4
+biplot(score=pca_res[:,0:2],
+       coeff=np.transpose(pca.components_[0:2, :]),
+       cat=y,
+       cmap="viridis",
+       coeff_labels=list(X.columns))
+plt.show()
