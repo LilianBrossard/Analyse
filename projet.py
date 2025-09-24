33b51b916825bee078e352fe605f0977aa9d8d97
AntoineToullec <148762390+AntoineToullec@users.noreply.github.com>
2025-09-24 09:56:01 +0200

    Initialize project with data loading and necessary imports for analysis

---
+++ projet.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+
+data = pd.read_csv('data.csv')
